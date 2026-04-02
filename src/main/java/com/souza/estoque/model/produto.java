a37caaea8ce480812277a35d57958d3107b66fd7
Souza-Ingrid <Ingridysolsousa@gmail.com>
2026-04-02 17:39:34 -0300

    feat: adiciona entity Produto e configura banco de dados

---
+++ src/main/java/com/souza/estoque/model/produto.java
@@ -0,0 +1,30 @@
+package com.souza.estoque.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+public class produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private Integer quantidade;
+
+    private Double preco;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public Integer getQuantidade() { return quantidade; }
+    public void setQuantidade(Integer quantidade) { this.quantidade = quantidade; }
+
+    public Double getPreco() { return preco; }
+    public void setPreco(Double preco) { this.preco = preco; }
+}
